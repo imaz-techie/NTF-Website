68eebd3cde9ac55329b96736519fdabe14b453a3
imaz-techie <mohammedimaz2264@gmail.com>
2025-05-15 16:50:01 +0530

    Dynamic time integration

---
+++ index.js
@@ -0,0 +1,18 @@
+setInterval(() => {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  if (hours < 10) {
+    hours = `0${seconds}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${seconds}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}, 1000);
